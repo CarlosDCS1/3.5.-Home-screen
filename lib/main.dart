81334d94293d44bffc94b0b217b3c731591e8155
CarlosDCS1 <charly.daniel13@gmail.com>
2024-05-05 12:05:00 -0600

    Initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,33 @@
+Claro, aquí te muestro cómo puedes implementar los puntos 3 y 4 en el código existente:
+
+```dart
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
   runApp(MyApp());
+  
+  // Llamamos a la función para obtener datos de prueba cuando se inicie la aplicación
+  getTestData();
+}
+
+Future<void> getTestData() async {
+  final String apiUrl = 'https://jsonplaceholder.typicode.com/posts';
+  
+  try {
+    final response = await http.get(Uri.parse(apiUrl));
+    
+    if (response.statusCode == 200) {
+      // La solicitud fue exitosa, puedes procesar los datos aquí
+      print('Datos recibidos: ${response.body}');
+    } else {
+      // La solicitud falló con un código de estado diferente de 200
+      print('Error en la solicitud: ${response.statusCode}');
+    }
+  } catch (e) {
+    // Hubo un error al realizar la solicitud
+    print('Error: $e');
+  }
 }
 
 class MyApp extends StatelessWidget {
@@ -55,3 +81,9 @@ class HomeScreen extends StatelessWidget {
     );
   }
 }
+
+// Punto 3: Envía al buzón de entregas la liga del código subido a un control de versiones y espera la retroalimentación del asesor(a).
+// No se puede implementar este punto en el código ya que es una tarea fuera del alcance de la aplicación.
+
+// Punto 4: Llamar a la función getTestData() en el lugar apropiado de tu aplicación, como en el main() o en cualquier otro lugar donde desees realizar una solicitud HTTP para practicar.
+// Ya se ha implementado la llamada a la función getTestData() en el main() para realizar una solicitud HTTP al iniciar la aplicación.
